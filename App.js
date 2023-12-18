12ad49912f0f507699d3d5fde4fd2f14a7539516
Ak4shKr <akashkmr7667@gmail.com>
2023-12-19 02:08:04 +0530

    commit message

---
+++ App.js
@@ -0,0 +1,7 @@
+import { StyleSheet, Text, View } from "react-native";
+import StartGameScreen from "./screens/StartGameScreen";
+export default function App() {
+  return <StartGameScreen />;
+}
+
+const styles = StyleSheet.create({});
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,46 @@
+import { TextInput, Button, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Comfirm</PrimaryButton>
+    </View>
+  );
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#4e0329",
+    borderRadius: 8,
+    elevation: 4,
+    //ios style
+    shadowColor: "rgba(255, 255, 255)",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
